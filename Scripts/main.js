0296ee505f93ebcf7d13f8ea5acdb5ecfc9b4e2b
zgardner11 <zgardner.cinema@gmail.com>
2023-07-25 17:35:37 -0700

    Mobile UI/JS

--- Scripts/main.js
+++ Scripts/main.js
@@ -6,7 +6,23 @@ const navMenu = document.querySelector('.navlinks-menu');
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     navMenu.classList.toggle('active');
+
+    // Toggle color of nav button on click
+
+    burger.classList.toggle('nav-invert');
+    // burger.classList.remove('nav-invert');
 })
+
+let viewportWidth = window.innerWidth;
+const logo = document.getElementById('logo');
+
+function logoInvert () {
+    if (viewportWidth <= 1024){
+        logo.setAttribute('src', 'Media/logowhite.PNG')
+    } 
+}
+
+logoInvert();
 
 // Drop down items
 
